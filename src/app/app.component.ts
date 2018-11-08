6d50424658e9548fa69d8cbb4b6989eaafefc169
Piotr Wawrzyniak <piotrek29100@gmail.com>
2018-11-08 18:41:33 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'age-recognition-client';
+
+  @ViewChild('videoElement') videoElement: any;
+  video: any;
+
+  isPlaying = false;
+
+  ngOnInit() {
+    this.video = this.videoElement.nativeElement;
+  }
+
+  start() {
+    this.initCamera({ video: true, audio: false });
+  }
+
+  initCamera(config: any) {
+    const browser = <any>navigator;
+
+    browser.getUserMedia = (browser.getUserMedia ||
+      browser.webkitGetUserMedia ||
+      browser.mozGetUserMedia ||
+      browser.msGetUserMedia);
+
+    browser.mediaDevices.getUserMedia(config).then(stream => {
+      this.video.src = window.URL.createObjectURL(stream);
+      this.video.play();
+    });
+
+  }
+
+  pause() {
+    this.video.pause();
+  }
+
+  resume() {
+    this.video.play();
+  }
 }
