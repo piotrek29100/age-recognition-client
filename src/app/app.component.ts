014400c4e4ed417a8b4a84524850913ac0de998d
Piotr Wawrzyniak <piotrek29100@gmail.com>
2018-11-08 18:48:50 +0100

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,37 @@
-import { Component } from '@angular/core';
-import { ViewChild } from '@angular/core';
-import { OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'age-recognition-client';
+export class AppComponent implements OnInit, AfterViewInit {
 
-  @ViewChild('videoElement') videoElement: any;
-  video: any;
+    @ViewChild('video')
+    public video: ElementRef;
 
-  isPlaying = false;
+    @ViewChild('canvas')
+    public canvas: ElementRef;
 
-  ngOnInit() {
-    this.video = this.videoElement.nativeElement;
-  }
+    public captures: Array<any>;
 
-  start() {
-    this.initCamera({ video: true, audio: false });
-  }
+    public constructor() {
+        this.captures = [];
+    }
 
-  initCamera(config: any) {
-    const browser = <any>navigator;
+    public ngOnInit() { }
 
-    browser.getUserMedia = (browser.getUserMedia ||
-      browser.webkitGetUserMedia ||
-      browser.mozGetUserMedia ||
-      browser.msGetUserMedia);
+    public ngAfterViewInit() {
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+                this.video.nativeElement.src = window.URL.createObjectURL(stream);
+                this.video.nativeElement.play();
+            });
+        }
+    }
 
-    browser.mediaDevices.getUserMedia(config).then(stream => {
-      this.video.src = window.URL.createObjectURL(stream);
-      this.video.play();
-    });
-
-  }
-
-  pause() {
-    this.video.pause();
-  }
-
-  resume() {
-    this.video.play();
-  }
+    public capture() {
+        const context = this.canvas.nativeElement.getContext('2d').drawImage(this.video.nativeElement, 0, 0, 640, 480);
+        this.captures.push(this.canvas.nativeElement.toDataURL('image/png'));
+    }
 }
